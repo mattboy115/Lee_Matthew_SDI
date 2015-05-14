f47e1b5159938e6d29456d78583c1c0be0fbb9cf
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-14 01:19:50 -0400

    Devised an expression.

--- Lee_Matthew_Expressions_Assignment/js/script.js
+++ Lee_Matthew_Expressions_Assignment/js/script.js
@@ -1,6 +1,7 @@
 //Lee_Matthew_5/13/15_Expressions_Assignment
 
-
 var grassHeight = prompt("What would you say is the height of your grass in inches? \nI charge more if it's really tall."); //Prompt for the height of grass.
 var yeardWidth = prompt("What is the width of your lawn in yards?"); //Prompt for the width of lawn.
 var yardLength = prompt("How far back does your lawn go in yards?"); //Promot for the length of lawn.
+
+console.log("Your yard is going to be $" + (yardLength * 0.5 + yeardWidth * 0.25 + grassHeight * 2) + " to cut."); //Output showing the cost of the lawn in US dollars.
