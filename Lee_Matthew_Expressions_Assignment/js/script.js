02670c6a47d2c9def2c14e6a719c75a22bae2330
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-14 01:21:55 -0400

    Added some confirming comments.

--- Lee_Matthew_Expressions_Assignment/js/script.js
+++ Lee_Matthew_Expressions_Assignment/js/script.js
@@ -4,4 +4,10 @@ var grassHeight = prompt("What would you say is the height of your grass in inch
 var yeardWidth = prompt("What is the width of your lawn in yards?"); //Prompt for the width of lawn.
 var yardLength = prompt("How far back does your lawn go in yards?"); //Promot for the length of lawn.
 
-console.log("Your yard is going to be $" + (yardLength * 0.5 + yeardWidth * 0.25 + grassHeight * 2) + " to cut."); //Output showing the cost of the lawn in US dollars.
+alert("Your yard will cost $" + (yardLength * 0.5 + yeardWidth * 0.25 + grassHeight * 2) + " to cut."); //Output showing the cost of the lawn in dollars.
+
+//I typed in 1, 40, and 50 and got $37.
+//I've tried 3, 200, and 340 and got $226.
+//I also tried 13, 35 and 60 and I got $64.75.
+
+//Looks like it works.
