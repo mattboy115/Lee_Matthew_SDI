e69dcb909c2e2856ff9e6360bd175bc394b378dc
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-14 01:13:22 -0400

    Added another variable and some comments.

--- Lee_Matthew_Expressions_Assignment/js/script.js
+++ Lee_Matthew_Expressions_Assignment/js/script.js
@@ -2,4 +2,5 @@
 
 
 var grassHeight = prompt("What would you say is the height of your grass in inches? \nI charge more if it's really tall."); //Prompt for the height of grass.
-var yeardWidth = prompt("What is the width of your lawn in yards?"); 
+var yeardWidth = prompt("What is the width of your lawn in yards?"); //Prompt for the width of lawn.
+var yardLength = prompt("How far back does your lawn go in yards?"); //Promot for the length of lawn.
