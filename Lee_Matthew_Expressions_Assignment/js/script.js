738f2523e7d53d0846fb8a11c5b0b62aac153408
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-14 01:11:26 -0400

    Added my first variable

--- Lee_Matthew_Expressions_Assignment/js/script.js
+++ Lee_Matthew_Expressions_Assignment/js/script.js
@@ -1,1 +1,4 @@
 //Lee_Matthew_5/13/15_Expressions_Assignment
+
+
+var grassHeight = prompt("What would you say is the height of your grass in inches? I charge more if it's really tall."); //Prompt for the height of grass.
