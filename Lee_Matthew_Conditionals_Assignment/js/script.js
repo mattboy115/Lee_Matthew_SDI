422abb7f72f377e0f4ef39745dc95f12f0755974
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-20 18:15:54 -0400

    Got the basic layout of the script file.

--- Lee_Matthew_Conditionals_Assignment/js/script.js
+++ Lee_Matthew_Conditionals_Assignment/js/script.js
@@ -1,3 +1,34 @@
 //Lee_Matthew_Conditionals_Assignment 5/19/15
 
-This is stuff.
+alert("This is a simple Javascript calculator! Just type in \nsimple artithmatic problems and you'll be presented with an alert with your answer!"); //Simple message to start the program off.
+
+var firstNumber = prompt("Type the first number of your problem."); //A prompt for the first number of the problem.
+var operatorType = prompt("Type in an artithmatic operator: \nPlus + \nMinus - \nMultiply * \nDivide /");
+var secondNumber = prompt("Type in the second number of your problem.");
+
+var resultOfProblem
+
+if(operatorType == "+") {
+	firstNumber + secondNumber = resultOfProblem
+}
+
+if(operatorType == "-") {
+	firstNumber - secondNumber = resultOfProblem
+}
+
+if(operatorType == "*") {
+	firstNumber * secondNumber = resultOfProblem
+}
+
+if(operatorType == "/") {
+	firstNumber / secondNumber = resultOfProblem
+}
+
+if(operatorType == "%") {
+
+}
+
+else() {
+	alert("That is not an acceptable action.")
+}
+
