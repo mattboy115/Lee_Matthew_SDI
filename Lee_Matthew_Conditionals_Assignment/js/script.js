0415acdd2111b5fcd566960ef949e71c8c423014
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-21 18:06:20 -0400

    Started different script file.
    Had to start the assignment over.

--- Lee_Matthew_Conditionals_Assignment/js/script.js
+++ Lee_Matthew_Conditionals_Assignment/js/script.js
@@ -1,34 +1,11 @@
-//Lee_Matthew_Conditionals_Assignment 5/19/15
+var yourPhoneBill = 
 
-alert("This is a simple Javascript calculator! Just type in \nsimple artithmatic problems and you'll be presented with an alert with your answer!"); //Simple message to start the program off.
+var howManyPhones = prompt("How many phones will you put on your plan? The initial cost is 20 dollars plus 50 dollars per phone.\nWe will add up to 5 phones per line!");
+var billBeforeData = prompt("You have added " howManyPhones " to your line. Your bill before data is $"howManyPhones * 50 + 20".";
 
-var firstNumber = prompt("Type the first number of your problem."); //A prompt for the first number of the problem.
-var operatorType = prompt("Type in an artithmatic operator: \nPlus + \nMinus - \nMultiply * \nDivide /");
-var secondNumber = prompt("Type in the second number of your problem.");
 
-var resultOfProblem
-
-if(operatorType == "+") {
-	firstNumber + secondNumber = resultOfProblem
-}
-
-if(operatorType == "-") {
-	firstNumber - secondNumber = resultOfProblem
-}
-
-if(operatorType == "*") {
-	firstNumber * secondNumber = resultOfProblem
-}
-
-if(operatorType == "/") {
-	firstNumber / secondNumber = resultOfProblem
-}
-
-if(operatorType == "%") {
+if(howManyPhones ** 50) {
 
 }
 
-else() {
-	alert("That is not an acceptable action.")
-}
-
+var howMuchData = prompt("We offer up to 8gb of data per month for our customers. Here is the price list.\n 1gb: $10\n3gb: $20");
