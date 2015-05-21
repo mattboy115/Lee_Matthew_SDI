6fa8e2a791d8ec8cc47f09158e045945b78bd914
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-21 18:55:53 -0400

    Made more changes to script file.

--- Lee_Matthew_Conditionals_Assignment/js/script.js
+++ Lee_Matthew_Conditionals_Assignment/js/script.js
@@ -1,3 +1,5 @@
+//Lee_Matthew_Conditionals_Assignment_5/21/2015
+
 var howManyPhones = prompt("How many phones will you put on your plan? The initial cost is 20 dollars plus 50 dollars per phone.\nWe will add up to 5 phones per line!"); //Asks how many phones will be put on your bill.
 var billBeforeData = howManyPhones * 50 + 20; //Bill with the number phones added
 
@@ -7,17 +9,19 @@ if(howManyPhones <= 5) {
 	alert("You have added " + howManyPhones + " to your line. Your bill before data is $" + billBeforeData + "."); //What happens when an acceptable value is entered.
 }
 
-if(howManyPhones < 1) {
+if(howManyPhones <= 0) {
 	howManyPhones = prompt("I'm sorry. We can't add negative phones to our data plans yet. Try next year perhaps."); //What happense if 0 or a negative number is entered.
 }
-if(isNan(howManyPhones)) {
+if(isNaN(howManyPhones)) {
 	howManyPhones = prompt("We're sorry. You're going to have to enter a whole number."); //What happense when a non number value is entered.
 }
 
-else {
-	howManyPhones = prompt("We're sorry. That's not an available option right now."); //What happens when anything else is entered.
+if(howManyPhones == " ") {
+	howManyPhones = prompt("You can't leave that space blank. Sorry.");
 }
 
 var howMuchData = prompt("We offer up to 8gb of data per month for our customers. How much data would you like?\n 1gb: $10\n3gb: $20\n5gb: ");
+
+var finalBill = howManyPhones + howMuchData;
 
 console.log("Your final bill is $" + finalBill + ".");
