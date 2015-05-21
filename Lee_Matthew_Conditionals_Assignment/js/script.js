df55d36b8755624522a3899d791cb91dc16f8587
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-21 18:17:54 -0400

    Made changes to script file

--- Lee_Matthew_Conditionals_Assignment/js/script.js
+++ Lee_Matthew_Conditionals_Assignment/js/script.js
@@ -1,11 +1,15 @@
 var yourPhoneBill = 
 
 var howManyPhones = prompt("How many phones will you put on your plan? The initial cost is 20 dollars plus 50 dollars per phone.\nWe will add up to 5 phones per line!");
-var billBeforeData = prompt("You have added " howManyPhones " to your line. Your bill before data is $"howManyPhones * 50 + 20".";
+var billBeforeData = howManyPhones * 50 + 20;
 
 
-if(howManyPhones ** 50) {
 
+if(howManyPhones <= 5) {
+	alert("You have added " howManyPhones " to your line. Your bill before data is $"billBeforeData".";
 }
 
+else {
+	alert("That's")
+}
 var howMuchData = prompt("We offer up to 8gb of data per month for our customers. Here is the price list.\n 1gb: $10\n3gb: $20");
