c347841162ea0f598852fafd872a438cc0e119bb
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-21 23:47:25 -0400

    Added to script file.

--- Lee_Matthew_Conditionals_Assignment/js/script.js
+++ Lee_Matthew_Conditionals_Assignment/js/script.js
@@ -10,23 +10,43 @@ if(howManyPhones <= 5 && howManyPhones >= 1) {
 }
 
 else {
-	howManyPhones = prompt("That's not an option we offer. Try again!")
+	howManyPhones = prompt("That's not an option we offer. Try again!"); //What happens when it's any other numerical value.
 }
 
 if(howManyPhones == "" || isNaN(howManyPhones)) {
 	howManyPhones = prompt("We're sorry. You're going to have to enter a whole number."); //What happense when a non number value or blank is entered.
 }
 
-if(addData = "No" ) {
-	console.log("Your final bill is " + billBeforeData + ".")
+if(addData == "No" ) {
+	console.log("Your final bill is " + billBeforeData + ".") //If the answer to the data question is "No". This code runs.
 }
 
-if(addData = "Yes" ) {
-	var howMuchData = prompt("We offer up to 8gb of data per month for our customers. How much data would you like?\n 1gb: $10\n3gb: $20\n5gb: $40\n8gb: $50");
+if(addData == "Yes" ) {
+	var howMuchData = prompt("We offer up to 8gb of data per month for our customers. How much data would you like?\n 1gb: $10\n3gb: $20\n5gb: $40\n8gb: $50"); //If the answer is "yes", then this code runs.
 }
 
+else {
+	var howMuchData = prompt("That's not an answer we're looking for."); //If the answer is anything else this code runs.
+}
 
+if(howMuchData == 1) {
+	finalBill = billBeforeData + 10;
+	console.log("Your final bill is $" + finalBill + "."); //The 
+}
 
-var finalBill = howManyPhones + howMuchData;
+if(howMuchData == 3) {
+	finalBill = billBeforeData + 20;
+	console.log("Your final bill is $" + finalBill + ".");
+}
+if(howMuchData == 5) {
+	finalBill = billBeforeData + 40;
+	console.log("Your final bill is $" + finalBill + ".");
+}
+if(howMuchData == 8) {
+	finalBill = billBeforeData + 50;
+	console.log("Your final bill is $" + finalBill + ".");
+}
 
-console.log("Your final bill is $" + finalBill + ".");
+else{
+
+}
