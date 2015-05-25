b5e1811922941c4875aea5a86833cdb96a12aa2e
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-25 02:31:35 -0400

    Finished the first activity. Started on second activity.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,6 +1,6 @@
 //Lee_Matthew_Functions_Worksheet_5/24/2015
 
-//Circumference
+//CIRCUMFERENCE
 
 var piRound = 3.14; //Pi rounded to the hundreths place. This variable is outside the functions because it's a contol and will always be the same.
 
@@ -12,12 +12,16 @@ function calcCirc() {  //Function for figuring circumference.
 
 function calcArea() {  //Function for area just because.
 	var radOfCircle = prompt("What is the radius of your circle?"); //User input for radius of the circle.
-	var area = piRound * math.pow(radOfCircle, 2); //Variable figuring the area of a circle.
+	var area = piRound * Math.pow(radOfCircle, 2); //Variable figuring the area of a circle.
 	console.log(area); //Console prints out the result, being the variable area.
 
 }
 
-calcCirc * calcArea
+calcCirc(); //Circumference function invoked.
+calcArea(); //Area function invoked.
 
-//Stung
+//I've tested out these two functions: 8 for the first one, getting 25.12 as an answer, and 4 for the other, getting 50.254 as an answer.
+//Checking those on the calculator, I am seeing they are correct.
+
+//STUNG
 
