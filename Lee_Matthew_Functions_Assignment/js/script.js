2284f3924baf81adf07c69d432e12f8196982d9c
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-27 06:08:31 -0400

    Added Prism Function

--- Lee_Matthew_Functions_Assignment/js/script.js
+++ Lee_Matthew_Functions_Assignment/js/script.js
@@ -1,2 +1,22 @@
 //Lee_Matthew_Functions_Assignment_5/26/2015
 
+//Function for Dimensions of a Sphere
+
+var 
+
+//Function for Dimensions of a Rectangular Prism
+
+alert
+
+var length = prompt("What's the length of this prism?");
+var width = prompt("What's the width?");
+var height = prompt("What is the height?");
+
+var calcVolume(length, width, height);
+
+function calcVolume(l, w, h) {
+	area = l * w * h;
+	console.log(area);
+}
+
+calcVolume;
