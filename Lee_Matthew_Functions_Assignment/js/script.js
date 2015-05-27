8740693343a5ed3f3517975804db365ef6f354b7
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-27 13:20:17 -0400

    Finished circle area function

--- Lee_Matthew_Functions_Assignment/js/script.js
+++ Lee_Matthew_Functions_Assignment/js/script.js
@@ -1,14 +1,27 @@
 //Lee_Matthew_Functions_Assignment_5/26/2015
 
-//Function for Dimensions of a Sphere
+//Function for Dimensions of a Circle
 
 var pi = 3.14; //pi rounded down to the hudredths place.
 var radius = prompt("Do you know the diameter of your sphere? Just half that is your radius! What's the radius?") //User input for the shape's radius.
 
-calcSphereVolume(pi, radius);
+calcCircleArea(pi, radius);
 
-function calcSphereVolume(p, r) {
-	var sphereArea = p * Math.pow(radius, 2);
+function calcCircleArea(p, r) {
+	var circleArea = p * Math.pow(r, 2);
+	console.log("The circle you've entered is " + circleArea + " square units!")
+}
+
+calcCircleArea;
+
+//Function for Volume of a Sphere
+
+var fourThirds = (4/3);
+
+calcSphereVolume(fourThirds, pi, radius);
+
+function calcSphereVolume() {
+
 }
 
 //Function for Dimensions of a Rectangular Prism
@@ -22,8 +35,8 @@ var height = prompt("What is the height?"); //User input for the shape's height.
 calcPrismVolume(length, width, height); //Storage for the above three variables.
 
 function calcPrismVolume(l, w, h) { //Function and its parameters.
-	var prismArea = l * w * h; //New variable made from multiplying those parameters.
-	console.log("Your prism is " + prismArea + " square units!"); //Output of shape's area.
+	var prismVolume = l * w * h; //New variable made from multiplying those parameters.
+	console.log("Your prism is " + prismVolume + " cubed units!"); //Output of shape's area.
 }
 
 calcPrismVolume; //Invoke the prism function.
