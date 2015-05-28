518ec858377e4abe6b6eab77538481948f3baac6
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-28 02:44:57 -0400

    Made small changes to the if statements

--- Lee_Matthew_Functions_Assignment/js/script.js
+++ Lee_Matthew_Functions_Assignment/js/script.js
@@ -1,53 +1,63 @@
-//Lee_Matthew_Functions_Assignment_5/26/2015
+			//Lee_Matthew_Functions_Assignment_5/26/2015
 
-//Main Function
+			//Main Function
 
-function theConjurer() {
-	prompt("Hi! This is my shape dimension calculator! I have three different shapes in this version but I can always add more! For now I have these three:\nCircle Area\nSphere Volume\nRectangular Prism Volume\n\nEnjoy!!!")
+			function theConjurer() {
+				firstQuestion = prompt("Hi! This is my shape dimension calculator! I have three different shapes in this version but I can always add more! For now I have these three:\nCircle Area\nSphere Volume\nRectangular Prism Volume\n\nEnjoy!!!"); //The answer to this prompt is supposed to spring one of the functions below.
 
-	if("Circle") {
-		
-	}
-}
+				if(firstQuestion === "Circle" || firstQuestion === "Circle Area") { //If the answer is one of these strings, then it triggers the circle area function.
+					calcCircleArea();
+				}
 
-//Function for Dimensions of a Circle
+				else if(firstQuestion === "Sphere" || firstQuestion === "Sphere Volume") { //This one triggers the sphere volume function.
+					calcSphereVolume();
+				}
 
-var pi = 3.14; //pi rounded down to the hudredths place.
-var radius = prompt("Do you know the diameter of your sphere? Just half that is your radius! What's the radius?") //User input for the shape's radius.
+				else if(firstQuestion === "Prism" || firstQuestion === "Rectangular Prism" || firstQuestion === "Prism Volume" || firstQuestion === "Rectangular Prism Volume") { //This one is for volume of a rectangular prism.
+					calcPrismVolume();
+				}
 
-calcCircleArea(pi, radius);
+				else {  //Anything else just brings up another prompt for the same answers.
+					firstQuestion = prompt("Sorry, that's not one of the options I have right now. Try again!");
+				}
+			}
 
-function calcCircleArea(p, r) {
-	var circleArea = p * Math.pow(r, 2);
-	console.log("The circle you've entered is " + circleArea + " square units!")
-}
+			theConjurer(); //To automatically bring up the starting function.
 
-//calcCircleArea; Invoke the circle area function
 
-//Function for Volume of a Sphere
+			//Function for Dimensions of a Circle
 
-var fourThirds = (4/3);
+			var pi = 3.14; //pi rounded down to the hudredths place.
 
-calcSphereVolume(fourThirds, pi, radius);
+			calcCircleArea(pi, radius); //Variable declarations.
 
-function calcSphereVolume(f, p, r) {
-	var sphereVolume = f * p * Math.pow(r, 3);
-	console.log("Your sphere is " sphereVolume " cubed units! Congradulations!")
-}
+			function calcCircleArea(p, r) { //
+				var circleArea = p * Math.pow(r, 2);
+				console.log("The circle you've entered is " + circleArea + " square units!");
+			}
 
-//calcSphereVolume; //Invoke the sphere volume function.
 
-//Function for Dimensions of a Rectangular Prism
+			//Function for Volume of a Sphere
 
-var length = prompt("What's the length of this prism?"); //User input for the shape's length.
-var width = prompt("What's the width?"); //User input for the shape's width.
-var height = prompt("What is the height?"); //User input for the shape's height.
+			calcSphereVolume(fourThirds, pi, radius);
 
-calcPrismVolume(length, width, height); //Storage for the above three variables.
+				var fourThirds = (4/3);
+				var radius = prompt("Do you know the diameter of your sphere? Just half that is your radius! What's the radius?"); //User input for the shape's radius.
 
-function calcPrismVolume(l, w, h) { //Function and its parameters.
-	var prismVolume = l * w * h; //New variable made from multiplying those parameters.
-	console.log("Your prism is " + prismVolume + " cubed units!"); //Output of shape's area.
-}
+			function calcSphereVolume(f, p, r) {
+				var sphereVolume = f * p * Math.pow(r, 3);
+				console.log("Your sphere is " + sphereVolume + " cubed units! Congradulations!");
+			}
 
-//calcPrismVolume; //Invoke the prism function.
+			//Function for Dimensions of a Rectangular Prism
+
+			var length = prompt("What's the length of this prism?"); //User input for the shape's length.
+			var width = prompt("What's the width?"); //User input for the shape's width.
+			var height = prompt("What is the height?"); //User input for the shape's height.
+
+			calcPrismVolume(length, width, height); //Storage for the above three variables.
+
+			function calcPrismVolume(l, w, h) { //Function and its parameters.
+				var prismVolume = l * w * h; //New variable made from multiplying those parameters.
+				console.log("Your prism is " + prismVolume + " cubed units!"); //Output of shape's area.
+			}
