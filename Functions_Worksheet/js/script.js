8cd4e739577ed3a60cdcbe540c25509d371a16d4
Matthew Lee <matthew_c_lee@yahoo.com>
2015-05-28 23:42:28 -0400

    Finished worksheet

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -3,10 +3,12 @@
 //CIRCUMFERENCE
 
 var piRound = 3.14; //Pi rounded to the hundreths place. This variable is outside the functions because it's a contol and will always be the same.
+var diaOfCircle = prompt("What is the diameter of your circle?"); //User input for diameter of the circle
 
-function calcCirc() {  //Function for figuring circumference.
-	var diaOfCircle = prompt("What is the diameter of your circle?"); //User input for diameter of the circle
-	var circumference = piRound * diaOfCircle; //Variable figuring the circumference.
+calcCirc(piRound, diaOfCircle);
+
+function calcCirc(p, d) {  //Function for figuring circumference.
+	var circumference = p * d; //Variable figuring the circumference.
 	console.log(circumference); //Console prints out the result, being the variable circumference.
 }
 
@@ -25,4 +27,12 @@ calcArea(); //Area function invoked.
 
 //STUNG
 
-var stingsPerPound = 8.666666667;
+var stingsPerPound = 8.666666667; //Number of stings per pound to kill an animal.
+var weightOfAnimal = prompt("What is the weight of the animal in lbs?"); //Input weight of animal.
+
+stingsToKill(stingsPerPound, weightOfAnimal); //Variable declaration
+
+function stingsToKill(s, w) { //Function for figuring how many stings would kill the animal with the input weight
+	var totalStings = s * w; //Code ran for figuring the number of stings
+	console.log("It would take " + totalStings + " bee stings to kill that animal.") //Prints out the value of totalStings.
+}
